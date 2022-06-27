fba5d627274a402ee6f1c4d259aa2dc7c1284ec7
Abdella <abdella.shukur2@gmail.com>
2022-06-27 17:42:58 -0400

    You can add 10 students names and it will print all of them when it gets to 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        try(Scanner obj = new Scanner(System.in)) {
+        String userName = "";
+        for(var i=0;i<10; i++) {
+        System.out.print("Enter student's name: ");
+        userName += obj.nextLine() + ", ";
+        System.out.println(userName);
+        }
+        if(userName.length() > 10) {
+        System.out.println("You printed " + userName);
+        }
+        } 
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 }
