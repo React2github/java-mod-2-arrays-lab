57c5d8d96a4381212777191c45ebd013fea47a3a
Abdella <abdella.shukur2@gmail.com>
2022-06-28 09:32:42 -0400

    created fixed array size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         try(Scanner obj = new Scanner(System.in)) {
-        String userName = "";
-        for(var i=0;i<10; i++) {
+        
+        String userName[] = {"", "", "", "", "", "", "", "", "", ""};
+        int count = 0;
+        for(var i=0;i<10; i++) {        
         System.out.print("Enter student's name: ");
-        userName += obj.nextLine() + ", ";
-        System.out.println(userName);
+        userName[i] = obj.nextLine();
+        count = count + 1;
+        
+        if(count == 10) {
+        System.out.println("You printed " + Arrays.toString(userName));
         }
-        if(userName.length() > 10) {
-        System.out.println("You printed " + userName);
         }
         } 
         catch (Exception e) {
